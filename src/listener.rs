d65aff2c25b2d718c94e6999a9c7d18eb50fe8d0
SpongyBacon <spongybacon@hotmail.co.uk>
2017-12-27 21:11:33 +0000

    borrow AudioPlayer mutably

--- src/listener.rs
+++ src/listener.rs
@@ -2,10 +2,10 @@ use ::prelude::*;
 use ::player::AudioPlayer;
 
 pub trait AudioPlayerListener: Send + Sync {
-    fn player_pause(&self, player: &AudioPlayer);
-    fn player_resume(&self, player: &AudioPlayer);
-    fn track_start(&self, player: &AudioPlayer, track: &str);
-    fn track_end(&self, player: &AudioPlayer, track: &str, reason: &str);
-    fn track_exception(&self, player: &AudioPlayer, track: &str, exception: &str);
-    fn track_stuck(&self, player: &AudioPlayer, track: &str, threshold: i64);
+    fn player_pause(&self, player: &mut AudioPlayer);
+    fn player_resume(&self, player: &mut AudioPlayer);
+    fn track_start(&self, player: &mut AudioPlayer, track: &str);
+    fn track_end(&self, player: &mut AudioPlayer, track: &str, reason: &str);
+    fn track_exception(&self, player: &mut AudioPlayer, track: &str, exception: &str);
+    fn track_stuck(&self, player: &mut AudioPlayer, track: &str, threshold: i64);
 }
