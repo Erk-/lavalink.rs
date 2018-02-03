fb13f61efe35d34964c03589842e5e40d5c857c0
SpongyBacon <spongybacon@hotmail.co.uk>
2018-02-03 13:32:54 +0000

    decode if stream, has a url, source

--- src/decoder.rs
+++ src/decoder.rs
@@ -12,7 +12,7 @@ macro_rules! move_cursor {
 }
 
 fn read_string(cursor: &mut Cursor<Vec<u8>>) -> Result<String> {
-    let size = cursor.read_u8()?;
+    let size = cursor.read_u16::<BigEndian>()?;
     let mut buf = vec![0u8; size as usize];
     cursor.read_exact(&mut buf)?;
     let string = String::from_utf8(buf)?;
@@ -27,6 +27,9 @@ pub struct DecodedTrack {
     pub author: String,
     pub length: u64,
     pub identifier: String,
+    pub stream: bool,
+    pub url: Option<String>,
+    pub source: String,
 }
 
 /// Decodes a binary lavaplayer track blob
@@ -39,30 +42,43 @@ pub fn decode_track(input: Vec<u8>) -> Result<DecodedTrack> {
     // gets the message size (we dont care)
     // let size = value as i32 & 0x3FFFFFFF;
 
-    move_cursor!(cursor, 2); // padding
+    move_cursor!(cursor, 2);
 
     let version = match flags & TRACK_INFO_VERSIONED {
         0 => 1,
         _ => cursor.read_u8()? & 0xFF
     } as u8;
 
-    move_cursor!(cursor, 3); // dont care
+    move_cursor!(cursor, 2); // dont care
 
     let title = read_string(&mut cursor)?;
-
-    move_cursor!(cursor, 1); // padding
 
     let author = read_string(&mut cursor)?;
 
     let length = cursor.read_u64::<BigEndian>()?;
 
-    move_cursor!(cursor, 1); // padding
-
     let identifier = read_string(&mut cursor)?;
 
-    Ok(DecodedTrack {
-        version, title, author, length, identifier
+    let stream = cursor.read_u8()? == 1;
+    let has_url = cursor.read_u8()? == 1;
 
+    let url = if has_url {
+        Some(read_string(&mut cursor)?)
+    } else {
+        let size = cursor.read_u8()? as u64;
+        move_cursor!(cursor, size);
+        None
+    };
+
+    let source = read_string(&mut cursor)?;
+    println!("source: {}", source);
+
+    let mut buf = Vec::new();
+    cursor.read_to_end(&mut buf)?;
+    println!("{:?}", buf);
+
+    Ok(DecodedTrack {
+        version, title, author, length, identifier, stream, url, source
     })
 }
 
